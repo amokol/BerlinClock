04fe1771e340b21509f07e230a930d8b858ca355
Angelo Okol <angelomiguelokol@yahoo.com>
2026-05-16 01:33:14 +0800

    Add project files.

---
+++ BerlinClock/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var hours = 1;
+        var minutes = 59;
+
+        Console.WriteLine($"{hours}:{minutes}");
+        var hourTime = hours / 5;
+        var hourMod = hours % 5;
+        var mins = (minutes / 5);
+        var minsMod = minutes % 5;
+        var hourCount = 1; // every hour count
+        var limit = 4; // limit for 4 columns
+        var minLimit = 11; // limit for minutes column
+        var minsCount = 3; // limit set for every 15 mins
+        var fullminsLimit = 5; //this is the count for 5 mins
+        // Start of setting hours
+        SetBerlinTime(hourTime, hourCount, limit);
+        SetBerlinTime(hourMod, hourCount, limit);
+        // End of setting Hours
+        // Start of seeting minutes
+        SetBerlinTime(mins, minsCount, minLimit);
+        SetBerlinTime(minsMod, fullminsLimit, limit);
+        // End of setting minutes
+    }
+
+    private static void SetBerlinTime(int time, int timeCount, int limit)
+    {
+        var berlinTime = new StringBuilder();
+        for (int i = 0; i < time; i++)
+        {
+            berlinTime.Append((i + 1) % timeCount == 0 ? "R" : "Y");
+        }
+        //padding to right all zero values
+        Console.WriteLine(berlinTime.ToString().PadRight(limit, 'O'));
+    }
+}
---
+++ BerlinClock/README.txt
@@ -0,0 +1,24 @@
+﻿Being passed the hours and minutes as integers, we want to get a response that corresponds 
+to the lights being off, red, or yellow in each row in the clock.
+----
+
+eg. given an input of
+hours = 13
+minutes = 23
+
+we'd expect a console output of:
+RROO - indication for every 5 hours set to R, otherwise set to O
+RRRO - indication of every 1 hour set to R, otherwise set to O
+YYRYOOOOOOO - every 5 minutes, change it to Y, for every 15 mins, set to R, otherwise if not yet in 5 or 15 minutes set to O
+YYYO -  count for every minutes, set to Y if it is in a minute, otherwise set to O
+
+Additional information on the timer:
+Hours:
+RROO -> this means that the 2 R is 10 hours
+RRRO -> this means that the 3 R is 3 hours
+With this information we get the 13 hour value
+
+Minutes:
+YYRYOOOOOOO -> this means that each Y consists of 5 minutes, the R is the 15 minute marker, this makes a total of 20 minutes.
+YYYO -> this means that each Y consists of 1 minute, otherwise set to O, this sample makes a total of 3 minutes.
+With this information for the minutes, we got a total of 13 minutes.
