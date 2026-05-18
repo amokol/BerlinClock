381b266aaa12aa1ecb20e46070e65dc9baf92ddc
Angelo Okol <angelomiguelokol@yahoo.com>
2026-05-18 21:28:23 +0800

    code update

--- BerlinClock/Program.cs
+++ BerlinClock/Program.cs
@@ -1,40 +1,39 @@
 ﻿using System;
-using System.Drawing;
 using System.Text;
 
 class Program
 {
     public static void Main(string[] args)
     {
-        var hours = 1;
-        var minutes = 59;
+        var hours = 13;
+        var minutes = 33;
 
         Console.WriteLine($"{hours}:{minutes}");
-        var hourTime = hours / 5;
-        var hourMod = hours % 5;
-        var mins = (minutes / 5);
-        var minsMod = minutes % 5;
-        var hourCount = 1; // every hour count
-        var limit = 4; // limit for 4 columns
-        var minLimit = 11; // limit for minutes column
-        var minsCount = 3; // limit set for every 15 mins
-        var fullminsLimit = 5; //this is the count for 5 mins
+        var fiveHourBlock = hours / 5;
+        var singleHourBlock = hours % 5;
+        var fiveMinuteBlock = minutes / 5;
+        var oneMinuteBlock = minutes % 5;
+        var hoursMark = 1; // interval for hours
+        var limit = 4; // interval for every 4 hours/minutes
+        var minuteLimitCount = 11; // limit for minutes column
+        var fifteenMinuteMark = 3; // interval for every 15 minutes
+        var fiveMinuteMark = 5; // interval for every 5 minutes
         // Start of setting hours
-        SetBerlinTime(hourTime, hourCount, limit);
-        SetBerlinTime(hourMod, hourCount, limit);
+        SetBerlinTime(fiveHourBlock, hoursMark, limit);
+        SetBerlinTime(singleHourBlock, hoursMark, limit);
         // End of setting Hours
         // Start of seeting minutes
-        SetBerlinTime(mins, minsCount, minLimit);
-        SetBerlinTime(minsMod, fullminsLimit, limit);
+        SetBerlinTime(fiveMinuteBlock, fifteenMinuteMark, minuteLimitCount);
+        SetBerlinTime(oneMinuteBlock, fiveMinuteMark, limit);
         // End of setting minutes
     }
 
-    private static void SetBerlinTime(int time, int timeCount, int limit)
+    private static void SetBerlinTime(int time, int intervalCount, int limit)
     {
         var berlinTime = new StringBuilder();
         for (int i = 0; i < time; i++)
         {
-            berlinTime.Append((i + 1) % timeCount == 0 ? "R" : "Y");
+            berlinTime.Append((i + 1) % intervalCount == 0 ? 'R' : 'Y');
         }
         //padding to right all zero values
         Console.WriteLine(berlinTime.ToString().PadRight(limit, 'O'));
